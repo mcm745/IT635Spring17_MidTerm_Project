c2cb977be848fd1a6979e65b1e2c5165e9adf436
mcm745 <mcm37@njit.edu>
2017-03-23 03:26:03 +0000

    modified script

--- show_historical_prices.sh
+++ show_historical_prices.sh
@@ -14,5 +14,13 @@ read -p "Enter a start date in a date range : " sdate
 
 read -p "Enter an end date in a date range : " edate
 
+if [ -z "$sdate" ] && [ -z "$edate" ] ; then
+   mysql -u$login -p$password  -t -e "use client_profile_db; call show_historical_prices ('$symbol',null,null);"
+elif [ -z "$sdate" ] ; then
+   mysql -u$login -p$password  -t -e "use client_profile_db; call show_historical_prices ('$symbol',null,'$edate');"
+elif [ -z "$edate" ] ; then
+   mysql -u$login -p$password  -t -e "use client_profile_db; call show_historical_prices ('$symbol','$sdate',null);"
+else
+   mysql -u$login -p$password  -t -e "use client_profile_db; call show_historical_prices ('$symbol','$sdate','$edate');"
+fi
 #mysql -uadmin -padmin_pass  -t -e "use client_profile_db; call show_historical_prices ('$1','$2','$3');"
-mysql -u$login -p$password  -t -e "use client_profile_db; call show_historical_prices ('$symbol','$sdate','$edate');"
